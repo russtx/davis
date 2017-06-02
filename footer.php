5ed3191c369a14136cc07aac423283044de2e98b
Russ berge <russ@toplinemediateam.com>
2017-06-02 13:53:37 -0400

    home page complete all media queries

--- footer.php
+++ footer.php
@@ -1,17 +1,31 @@
 <footer>
-  <?php bloginfo( 'name' ); ?>
-  <div class="social">
-  <?php
-  // social links, using the svgs inside the icons folder
-  // sample icons are from https://dribbble.com/shots/2089345-Nucleo-Free-Social-Icons
-  social_links('facebook');
-  social_links('twitter');
-  social_links('youtube');
-  social_links('instagram');
-  ?>
+  <div class="blueFooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col sm-8  footerForm">
+          <h2>EMPLOYEE LOGIN</h2>
+          <?php echo do_shortcode('[contact-form-7 id="54" title="Employee Login"]'); ?>
 
-  </div>
-
+        </div>
+      </div><!-- row -->
+    </div><!-- container -->
+  </div><!-- blueFooter -->
+  <div class="whiteFooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-2 col-sm-2 footerLogo">
+          <a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" alt="Footer Logo" /></a>
+        </div>
+        <div class="col-xs-6 col-sm-8 footerMenu">
+            <?php wp_nav_menu(); ?>
+        </div>
+        <div class="col-xs-4 col-sm-2 footerSocials">
+          <a href="<?php the_field('twitter', 'option'); ?>" target="_blank" class="twitter"></a>
+          <a href="<?php the_field('facebook', 'option'); ?>" target="_blank" class="facebook"></a>
+        </div>
+      </div><!-- row -->
+    </div><!-- container -->
+  </div><!-- whiteFooter -->
 <?php wp_footer(); ?>
 </footer>
 
--- front-page.php
+++ front-page.php
@@ -193,6 +193,7 @@
 							</div>
         			<?php endwhile; endif; wp_reset_postdata(); ?>
 						</div>
+						<div class="clearfix"></div>
 						<a href="#" class="btn-main left-side">OUR BLOG</a>
 						<a href="#" class="btn-main right-side">SUBSCRIBE</a>
 					</div><!-- row -->
@@ -214,10 +215,35 @@
 
 
 	<div class="section" id="section5">
-		<div class="contact-us">
-			<h1>6th section elements</h1>
-			<p>Create your own headers and footers</p>
-		</div>
+		<div class="contactBackground">
+			<div class="contact-us">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-4 contactTitle">
+							<h1>CONTACT US</h1>
+						</div>
+						<div class="col-xs-12 col-sm-4 contactInfo">
+							<div class="col-xs-6 col-sm-12">
+								<h3>ADDRESS</h3>
+								<p>
+									475 AQUIA TOWNE CENTER DRIVE<BR />
+									SUITE #401 STAFFORD, VA 22504
+								</p>
+							</div>
+							<div class="col-xs-6 col-sm-12">
+								<h3>PHONE</h3>
+								<p>540. 548. 5980</p>
+								<h3>FAX</h3>
+								<p>540 542 3079</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-4 contactForm">
+							<?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]'); ?>
+						</div>
+					</div><!--row -->
+				</div><!-- container -->
+			</div><!-- contact-us -->
+		</div><!-- contactBackground -->
 		<?php get_footer(); ?>
 	</div>
 </div><!-- fulpage -->
